6f72edd8863435c5921a48cb655028d02ef649be
Lara-5 <lkrvavica@pmfst.hr>
2025-01-05 04:02:34 +0100

    dodana ruta za prebrojavanje slobodnih mjesta

--- server.js
+++ server.js
@@ -6,7 +6,31 @@ const port = 3000;
 
 app.use(express.json());
 
-
+//  ********** broj slobodnih mjesta
+app.get('/slobodna-mjesta/:id', async (req, res) => { 
+    const parkingId = parseInt(req.params.id, 10);
+  
+    if (isNaN(parkingId)) {
+      return res.status(400).json({ error: 'Neispravan parking ID.' });
+    }
+  
+    try {
+      const { data, error } = await supabase.rpc('slobodna_mjesta', { parking_id: parkingId } );
+      if (error) {
+        return res.status(500).json({ error: 'Greška prilikom dohvaćanja podataka' });
+      }
+  
+      const slobodnaMjesta = data[0]?.slobodna_mjesta; 
+      if (slobodnaMjesta === undefined) {
+        return res.status(500).json({ error: 'Neispravan odgovor RPC funkcije' });
+      }
+      res.json({ slobodna_mjesta: slobodnaMjesta });
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Nepoznata greška' });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server radi na http://localhost:${port}`);
