f6fdc31aa6534f132965aacedfcba5ef9438e85f
ivamrjn <imarijan@pmfst.hr>
2025-01-23 00:36:40 +0100

    dodani end2end testovi za prvi sprint

---
+++ testovi/end2end/buy-ticket.cy.js
@@ -0,0 +1,125 @@
+describe('Buy Parking Ticket Test', () => {
+  
+    beforeEach(() => {
+      
+      cy.visit('http://127.0.0.1:5500/buy-ticket.html');
+    });
+
+    it('should allow user to select parking location and duration', () => {
+      
+      cy.get('#plateNumber').type('ZG12345');
+
+     
+      cy.get('.next-step').click({multiple: true});
+
+      
+      cy.get('#parkingLocation').select('2');  
+
+     
+      cy.get('#duration').select('day'); 
+
+      
+      cy.get('.next-step').filter(':visible') 
+      .click();  
+
+      
+      cy.get('#parkingFee').should('not.contain.text', 'NaN');  
+      cy.get('#totalPrice').should('not.contain.text', 'NaN');  
+      cy.get('#parkingFee').should('contain.text', '$');  
+      cy.get('#totalPrice').should('contain.text', '$');  
+
+      
+      cy.get('.submit-button')
+      .filter(':visible')  
+      .click();  
+    
+      cy.get('#cardNumber').should('be.visible');
+      cy.get('#expiryDate').should('be.visible');
+      cy.get('#cvv').should('be.visible');
+    });
+  });
+ describe('Buy Parking Ticket Test', () => {
+
+   beforeEach(() => {
+     
+     cy.visit('http://127.0.0.1:5500/buy-ticket.html');
+   });
+
+   it('should prevent user from clicking Next without entering plate number', () => {
+
+     cy.get('.next-step').filter(':visible').click();
+
+  
+
+     cy.get('.form-step.active')
+       .should('have.length', 1); 
+   });
+
+ });
+describe('Buy Parking Ticket Test', () => {
+
+  beforeEach(() => {
+    
+    cy.visit('http://127.0.0.1:5500/buy-ticket.html');
+  });
+
+  it('should trigger TypeError when attempting to proceed without selecting parking location', () => {
+    
+    cy.get('#plateNumber').type('ZG12345');
+
+    cy.get('.next-step').click({ multiple: true });
+
+   
+    cy.get('#duration').select('day');
+
+   
+    cy.get('#parkingLocation').should('have.value', '');  
+
+    
+    cy.get('#parkingFee').should('contain.text', '$0.00');  
+    cy.get('#totalPrice').should('contain.text', '$0.00');  
+
+    
+    cy.on('uncaught:exception', (err) => {
+      
+      expect(err.message).to.include('Cannot read properties of null (reading \'toFixed\')');
+     
+      return false;
+    });
+
+   
+    cy.get('.next-step').filter(':visible').click();
+
+  });
+});
+
+
+  describe('Buy Parking Ticket Test', () => {
+
+    beforeEach(() => {
+      
+      cy.visit('http://127.0.0.1:5500/buy-ticket.html');
+    });
+
+    it('should not allow user to proceed without selecting duration', () => {
+     
+      cy.get('#plateNumber').type('ZG12345');
+
+      
+      cy.get('.next-step').click({multiple: true});  
+      
+      cy.get('#parkingLocation').select('2');  
+
+  
+    
+
+      
+      cy.get('.next-step').filter(':visible').click();
+    
+    
+
+      
+      cy.get('#parkingFee').should('contain.text', '$1.00');
+      cy.get('#totalPrice').should('contain.text', '$1.00');
+    });
+  });
---
+++ testovi/end2end/cancel-ticket.cy.js
@@ -0,0 +1,64 @@
+describe('Cancel Ticket Test', () => {
+
+    beforeEach(() => {
+      
+      cy.visit('http://127.0.0.1:5500/cancel-ticket.html');
+    });
+  
+    it('should delete a ticket successfully with a valid vehicle ID', () => {
+      const vehicleId = 'ST55895'; 
+  
+      cy.get('#vehicleIdDelete').type(vehicleId);
+  
+      cy.get('#vehicleIdDelete').should('have.value', vehicleId);
+  
+    
+      cy.intercept('DELETE', `http://localhost:3000/kupljena-karta/${vehicleId}`, {
+        statusCode: 200,
+        body: { message: 'Ticket successfully deleted.' },  
+      }).as('deleteTicket');
+  
+     
+      cy.get('#deleteTicketButton').click();
+  
+      cy.wait('@deleteTicket');
+  
+      cy.get('#deleteMessage').should('have.text', 'Ticket successfully deleted.');
+      cy.get('#deleteMessage').should('have.css', 'color', 'rgb(0, 128, 0)');  
+    });
+  
+   
+  
+  });
+  describe('Cancel Ticket Test - Vehicle ID Not Found', () => {
+
+    beforeEach(() => {
+      
+      cy.visit('http://127.0.0.1:5500/cancel-ticket.html');
+    });
+  
+    it('should show an error message if the vehicle ID is not found in the database', () => {
+      const nonExistentVehicleId = 'XX99999';  
+  
+      cy.get('#vehicleIdDelete').type(nonExistentVehicleId);
+  
+    
+      cy.get('#vehicleIdDelete').should('have.value', nonExistentVehicleId);
+  
+     
+      cy.intercept('DELETE', `http://localhost:3000/kupljena-karta/${nonExistentVehicleId}`, {
+        statusCode: 404,
+        body: { message: 'Ticket doesnt exist.' },  
+      }).as('deleteTicket');
+  
+      cy.get('#deleteTicketButton').click();
+  
+      cy.wait('@deleteTicket');
+  
+     
+      cy.get('#deleteMessage').should('have.text', 'Ticket doesnt exist.');
+      
+    });
+  
+  });
+  
